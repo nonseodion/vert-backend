aa49700c25cb68619aac1d418b949696c87afa3f
nonseodion <nonseifebhor@gmail.com>
2023-07-04 15:51:20 +0100

    added blockchain config

---
+++ src/services/blockchain/config.blockchain.ts
@@ -0,0 +1,33 @@
+import { createPublicClient, http, Client } from "viem";
+import { localhost, bsc, bscTestnet } from "viem/chains";
+
+
+const localhostClient = createPublicClient({
+  chain: localhost,
+  transport: http()
+});
+
+const bscClient = createPublicClient({
+  chain: bsc,
+  transport: http()
+});
+
+const bscTestnetClient = createPublicClient({
+  chain: bscTestnet,
+  transport: http()
+});
+
+type SupportedClient = "bsc" | "bscTestnet" | "localhost";
+
+const clients = {
+  bsc: bscClient, bscTestnet: bscTestnetClient, localhost: localhostClient
+}
+
+function getClient(client: SupportedClient) {
+  return clients[client];
+} 
+
+
+export {
+  getClient
+}
