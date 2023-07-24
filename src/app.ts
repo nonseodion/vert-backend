b41efed748ec63b45b56f7eea7599ededb77c87c
nonseodion <nonseifebhor@gmail.com>
2023-07-24 21:35:02 +0100

    added cors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { createServer } from "http"
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cors from "cors";
 import "./utils/setupEnv"
 
 import banksRouter from "./routes/banks/banks.routes";
@@ -12,8 +13,9 @@ import setupSocket from "./sockets/setup.socket";
 const app = express();
 app.use(morgan("tiny"));
 app.use(helmet());
+app.use(cors());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.use("/banks", banksRouter);
 
